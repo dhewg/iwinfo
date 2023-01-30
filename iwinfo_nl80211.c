dhewg/iwinfo

commit 6194aaf05244241d2f51c471a43584f8d80b64ce

nl80211: simplify iterating over phy's devices
There's no need to iterate over all devices in /sys/class/net and check
if the phy index matches, sysfs already provides per phy devices.

Signed-off-by: Andre Heider <a.heider@gmail.com>

iwinfo_nl80211.c
@@ -826,24 +826,21 @@ static char * nl80211_phy2ifname(const char *ifname)
 
 	memset(nif, 0, sizeof(nif));
 
-	if ((d = opendir("/sys/class/net")) != NULL)
+	snprintf(buffer, sizeof(buffer),
+	         "/sys/class/ieee80211/phy%d/device/net", phyidx);
+
+	if ((d = opendir(buffer)) != NULL)
 	{
 		while ((e = readdir(d)) != NULL)
 		{
 			snprintf(buffer, sizeof(buffer),
-			         "/sys/class/net/%s/phy80211/index", e->d_name);
+			         "/sys/class/net/%s/ifindex", e->d_name);
 
-			if (nl80211_readint(buffer) == phyidx)
+			if ((cifidx = nl80211_readint(buffer)) >= 0 &&
+			    ((ifidx < 0) || (cifidx < ifidx)))
 			{
-				snprintf(buffer, sizeof(buffer),
-				         "/sys/class/net/%s/ifindex", e->d_name);
-
-				if ((cifidx = nl80211_readint(buffer)) >= 0 &&
-				    ((ifidx < 0) || (cifidx < ifidx)))
-				{
-					ifidx = cifidx;
-					strncpy(nif, e->d_name, sizeof(nif) - 1);
-				}
+				ifidx = cifidx;
+				strncpy(nif, e->d_name, sizeof(nif) - 1);
 			}
 		}
 

commit 60183c215a223bc0f4bfc2e6f6e9b01d8c99e43e

nl80211: restore iterating over all devices in nl80211_phy2ifname()
This reverts to the earlier behaviour, because:
* phys can be renamed, which breaks hardcoding "phy%d"
* /sys/class/ieee80211/*/device can return networks of other phys,
  since "device" is a symlink which can have multiple phys

The earlier behaviour fixes both points.

Fixes: 6194aaf0 "nl80211: simplify iterating over phy's devices"
Fixes: #11902
Signed-off-by: Andre Heider <a.heider@gmail.com>

iwinfo_nl80211.c
@@ -825,13 +825,15 @@ static char * nl80211_phy2ifname(const char *ifname)
 
 	memset(nif, 0, sizeof(nif));
 
-	snprintf(buffer, sizeof(buffer),
-	         "/sys/class/ieee80211/phy%d/device/net", phyidx);
-
-	if ((d = opendir(buffer)) != NULL)
+	if ((d = opendir("/sys/class/net")) != NULL)
 	{
 		while ((e = readdir(d)) != NULL)
 		{
+			snprintf(buffer, sizeof(buffer),
+			         "/sys/class/net/%s/phy80211/index", e->d_name);
+			if (nl80211_readint(buffer) != phyidx)
+				continue;
+
 			snprintf(buffer, sizeof(buffer),
 			         "/sys/class/net/%s/ifindex", e->d_name);
 			cifidx = nl80211_readint(buffer);